7f0cf56b06637492a1a34ce1a3761c35e96355ae
Francisco Fernandes <francisco.joao2000@gmail.com>
2022-05-15 17:32:56 +0100

    Added functionality to project controller

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,9 +1,11 @@
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/components/Project.module.css";
+import ProjectController from "./ProjectController";
 
 const Project = () => {
+  const [current, setCurrent] = useState(1);
+
   return (
     <div className={styles.projectContainer}>
       <div className={styles.descriptionContainer}>
@@ -15,15 +17,11 @@ const Project = () => {
           sed ipsum bibendum varius et a nisi. Pellentesque euismod volutpat
           dictum. Nunc vel est malesuada enim accumsan tincidunt.
         </p>
-        <div className={styles.controller}>
-          <div className={styles.square}>
-            <FontAwesomeIcon icon={faAngleLeft} size="sm" />
-          </div>
-          <p>1 / 3</p>
-          <div className={styles.square}>
-            <FontAwesomeIcon icon={faAngleRight} size="sm" />
-          </div>
-        </div>
+        <ProjectController
+          setCurrent={setCurrent}
+          current={current}
+          total={3}
+        />
       </div>
       <div className={styles.imageContainer}>
         <Image
