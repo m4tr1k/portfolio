1424a404db1c2b60a9607fba5e91a979b38a07e1
Francisco Fernandes <francisco.joao2000@gmail.com>
2023-02-13 23:52:37 +0000

    Homepage is now responsive

--- sections/HomeSection.tsx
+++ sections/HomeSection.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import styles from "../styles/sections/HomeSection.module.css";
 import Section from "../components/Section";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,7 +6,7 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const HomeSection = () => {
   return (
-    <React.Fragment>
+    <div style={{ position: "relative" }}>
       <Section id="0" className={styles.homeSection}>
         <div className={styles.homeContent}>
           <h1>
@@ -18,18 +17,22 @@ const HomeSection = () => {
           <div className={styles.separator} />
           <p>Explore my path as a software engineer and my newest adventures</p>
         </div>
-        <div className={styles.scrollDown}>
-          <div></div>
-          <p>Take a Look</p>
+        <div className={styles.bottomBar}>
+          <div className={styles.scrollDown}>
+            <div></div>
+            <p>Take a Look</p>
+          </div>
+          <a
+            className={styles.photoCredits}
+            href="https://www.instagram.com/inesandhercamera/"
+            target="_blank"
+          >
+            <p>
+              <span>Photo by </span>Inês Basto
+            </p>
+            <FontAwesomeIcon icon={faInstagram} className={styles.instagram} />
+          </a>
         </div>
-        <a
-          className={styles.photoCredits}
-          href="https://www.instagram.com/inesandhercamera/"
-          target="_blank"
-        >
-          <p>Photo by Inês Basto</p>
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
       </Section>
       <Image
         alt="Background Image"
@@ -38,7 +41,7 @@ const HomeSection = () => {
         priority
         className={styles.backgroundImage}
       />
-    </React.Fragment>
+    </div>
   );
 };
 
