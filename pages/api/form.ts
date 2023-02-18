3e03d3f8cd7b4db2b782649498614b6d7170e2ed
Francisco Fernandes <francisco.joao2000@gmail.com>
2023-02-18 11:30:52 +0000

    Rewrite nodemailer logic

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
+import nodemailer, { SentMessageInfo } from "nodemailer";
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,34 +7,53 @@ export default async function handler(
 ) {
   const body = JSON.parse(req.body);
   const { name, email, message } = body;
-  let errorMessage: string = "";
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    port: 465,
+    host: "smtp.gmail.com",
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
   });
 
-  const mailOptions = {
+  const server = await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (success) {
+        resolve(success);
+      }
+      reject(error);
+    });
+  });
+  if (!server) {
+    res.status(500).json({ error: "Error failed" });
+  }
+
+  const mailData = {
     from: process.env.EMAIL,
     to: process.env.EMAIL,
     subject: `Message from ${name} - ${email}`,
     text: message,
   };
 
-  transporter.sendMail(mailOptions, function (error) {
-    if (error) {
-      errorMessage = error.message;
-    }
+  const success = await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData).then(
+      (value: SentMessageInfo) => {
+        if (value.response.includes("250")) {
+          resolve(true);
+        }
+      },
+      (reason: any) => {
+        reject();
+      }
+    );
   });
 
-  // Found the name.
-  // Sends a HTTP success code
-  if (errorMessage !== "") {
-    return res.status(500).json({ error: errorMessage });
-  } else {
-    return res.status(200).json({ data: `Message Sent!` });
+  if (!success) {
+    res.status(500).json({ error: "Error sending email" });
   }
+  res.status(200).json({ success: success });
 }
