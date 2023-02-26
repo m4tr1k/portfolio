a8507417b030adc33d054e175fed3bdea3f0123a
Francisco Fernandes <francisco.joao2000@gmail.com>
2023-02-26 00:39:07 +0000

    Improved loading screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { Cabin, Titillium_Web, Courgette } from "@next/font/google";
 import { ReCaptchaProvider } from "next-recaptcha-v3";
-import { useEffect } from "react";
 import Spinner from "../components/Spinner";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { appWithTranslation } from "next-i18next";
@@ -21,25 +20,26 @@ library.add(faLinkedin, faGithub, faTwitter, faCheck, faXmark);
 //Google Fonts config
 const titillium_web = Titillium_Web({
   weight: ["200", "600"],
+  display: "swap",
 });
-const cabin = Cabin();
-const courgette = Courgette({ weight: "400" });
+const cabin = Cabin({ display: "swap" });
+const courgette = Courgette({ weight: "400", display: "swap" });
+
+if (typeof window !== "undefined") {
+  window.addEventListener("load", () =>
+    setTimeout(() => {
+      const loader = document.querySelector(".globalLoader") as HTMLElement;
+      const main = document.getElementById("main");
+      if (loader && main) {
+        loader.style.display = "none";
+        main.style.animation = "appear 0.4s ease-in-out";
+        main.style.opacity = "1";
+      }
+    }, 3000)
+  );
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    setTimeout(() => {
-      if (typeof window !== "undefined") {
-        const loader = document.querySelector(".globalLoader") as HTMLElement;
-        const main = document.getElementById("main");
-        if (loader && main) {
-          loader.style.display = "none";
-          main.style.animation = "appear 0.4s ease-in-out";
-          main.style.opacity = "1";
-        }
-      }
-    }, 3000);
-  }, []);
-
   return (
     <>
       <style jsx global>{`
@@ -52,6 +52,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ReCaptchaProvider>
         <div className="globalLoader">
           <Spinner />
+          <p>Loading...</p>
         </div>
         <main id="main">
           <Component {...pageProps} />
